215176da7f20ff89dde20ee0f9e251179c18744a
Nathan Wiles <ntwiles@gmail.com>
2021-11-02 18:27:20 -0400

    Organize imports.

--- CustomMongoAttributes/MongoWebJobsBuilderExtensions.cs
+++ CustomMongoAttributes/MongoWebJobsBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.DependencyInjection;
 
--- CustomMongoAttributes/Trigger/MongoValueBinder.cs
+++ CustomMongoAttributes/Trigger/MongoValueBinder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+
 using Microsoft.Azure.WebJobs.Host.Bindings;
 
 namespace WebJobs.Extension.Mongo
