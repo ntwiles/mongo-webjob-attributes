920cdd2047b19c28181fc3962fa9aa883c940aff
Nathan Wiles <nathan@nathans-air.home>
2021-11-01 21:50:40 -0400

    Convert trigger project to library.

--- MongoTriggerSample/MongoTriggerSample.cs
+++ MongoTriggerSample/MongoTriggerSample.cs
@@ -3,14 +3,13 @@ using CustomMongoAttributes;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
-
 namespace MongoTriggerSample
 {
     public static class MongoTriggerSample
     {
         [FunctionName("MongoTriggerSample")]
         public static void Run(
-            [MongoTrigger(Connection = "NatsConnection", QueueGroup = "SampleGroup")] string message,
+            [MongoTrigger(Connection = "MongoConnection", QueueGroup = "SampleGroup")] string message,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
